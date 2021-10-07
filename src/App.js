e9b08f343384e4b9e3886316e2ea5ab8418f0511
alex-j-k <63852006+alex-j-k@users.noreply.github.com>
2021-10-07 14:36:05 +1000

    resolve prob

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
     <div className="App">
       <h1>This is the first commit</h1>
       <h2>Second commit</h2>
-      <h3>Third commit</h3>
-      <h4>fourth commit</h4>
+      <h4>fourth commit</h4> 
+      <p>more changes</p>
       </div>
   );
 }
